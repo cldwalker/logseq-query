a969ae7df165ec5a614394adb8a8c43b85802161
Gabriel Horner <gabriel.horner@gmail.com>
2022-04-28 22:31:15 -0400

    CI job should fail when tests fail

--- test/test_runner.cljs
+++ test/test_runner.cljs
@@ -5,8 +5,11 @@
             [cldwalker.logseq-query.datascript-test]))
 
 (defn init []
-  (t/run-tests 'cldwalker.logseq-query.datascript-test
-               'cldwalker.logseq-query.queries-test))
+  (let [{:keys [error fail]}
+        (t/run-tests 'cldwalker.logseq-query.datascript-test
+                     'cldwalker.logseq-query.queries-test)]
+    (when (pos? (+ error fail))
+      (throw (ex-info "Tests failed" {:babashka/exit 1})))))
 
 (when (= nbb/*file* (:file (meta #'init)))
   (init))
